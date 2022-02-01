13984677901bcd415b54c438ad40e0743d340df4
Guillaume-G9 <ggonthier@live.fr>
2022-02-01 22:15:53 +0100

    js changes

--- katasJS6.js
+++ katasJS6.js
@@ -1,6 +1,6 @@
 const d = new Date();
 
-const event = new Date();
+const date = new Date();
 const options = { weekday: 'long', year: 'numeric', month: 'numeric', day: 'numeric' };
 
-document.getElementById("date").innerHTML = ("Date Js : " + event.toLocaleDateString(undefined, options));
+document.getElementById("date").innerHTML = ("Date Js : " + date.toLocaleDateString(undefined, options));
